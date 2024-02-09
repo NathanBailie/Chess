b84bd7f26876a9e1bf6f027291b51d570b38724b
Nathan <antnefedov33@gmail.com>
2024-02-09 12:08:33 +0300

    the possibility of players moves toggling is realized

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
 import { BoardComponent } from 'entities/BoardComponent';
 import { useEffect, useState } from 'react';
 import { Board } from 'shared/models/Board';
+import { Colors } from 'shared/models/Colors';
+import { Player } from 'shared/models/Player';
 
 const App = () => {
     const [board, setBoard] = useState(new Board());
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
     useEffect(() => {
-        restart()
+        restart();
+        setCurrentPlayer(whitePlayer);
     }, [])
 
     function restart() {
@@ -15,11 +21,20 @@ const App = () => {
         newBoard.addFigures();
         setBoard(newBoard);
     }
+
+    function togglePlayer(): void {
+        const player = currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer;
+
+        setCurrentPlayer(player);
+    }
+
     return (
         <div className="app">
             <BoardComponent
                 board={board}
                 setBoard={setBoard}
+                currentPlayer={currentPlayer}
+                togglePlayer={togglePlayer}
             />
         </div>
     )
--- src/entities/BoardComponent/ui/BoardComponent.tsx
+++ src/entities/BoardComponent/ui/BoardComponent.tsx
@@ -4,15 +4,18 @@ import { CellComponent } from 'entities/CellComponent';
 import { type Board } from 'shared/models/Board';
 import cls from './BoardComponent.module.scss';
 import { type Cell } from 'shared/models/Cell';
+import { type Player } from 'shared/models/Player';
 
 interface BoardComponentProps {
     className?: string
     board: Board
     setBoard: (board: Board) => void
+    currentPlayer: Player | null
+    togglePlayer: () => void
 }
 
 export const BoardComponent = (props: BoardComponentProps) => {
-    const { className, board, setBoard } = props;
+    const { className, board, setBoard, currentPlayer, togglePlayer } = props;
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
     useEffect(() => {
@@ -22,9 +25,12 @@ export const BoardComponent = (props: BoardComponentProps) => {
     function click(cell: Cell) {
         if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell);
+            togglePlayer()
             setSelectedCell(null);
         } else {
-            setSelectedCell(cell);
+            if (cell.figure?.color === currentPlayer?.color) {
+                setSelectedCell(cell);
+            }
         }
     }
 
